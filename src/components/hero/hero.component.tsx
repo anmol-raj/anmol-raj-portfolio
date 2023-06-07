3270a6496de3d3ab16f14c5737c36a7bff2b26c2
Anmol Raj <anmol@tykeinvest.com>
2023-06-07 18:33:38 +0530

    hero section done..

--- src/components/hero/hero.component.tsx
+++ src/components/hero/hero.component.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { styles } from "../../styles";
-import { ComputersCanvas } from "../canvas";
+import ComputersCanvas from "../canvas/Computers";
 
 const Hero: React.FC<{}> = ({}) => {
   return (
@@ -23,7 +23,7 @@ const Hero: React.FC<{}> = ({}) => {
           </p>
         </div>
       </div>
-      {/* <ComputersCanvas /> */}
+      <ComputersCanvas />
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
