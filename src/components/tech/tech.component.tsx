f20a11f4ef10fea6e32a18e77d8d4ac0e2c7e7e2
Anmol Raj <anmol@tykeinvest.com>
2023-06-10 16:37:10 +0530

    projects section done

--- src/components/tech/tech.component.tsx
+++ src/components/tech/tech.component.tsx
@@ -1,5 +1,17 @@
+import BallCanvas from "../canvas/Ball";
+import { SectionWrapper } from "../../hoc";
+import { technologies } from "../../constants";
+
 const Tech: React.FC<{}> = ({}) => {
-  return <> Tech Section </>;
+  return (
+    <div className="flex flex-row flex-wrap gap-10 justify-center">
+      {technologies.map((technology) => (
+        <div key={technology.name} className="w-28 h-28">
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default Tech;
+export default SectionWrapper(Tech, "tech");
